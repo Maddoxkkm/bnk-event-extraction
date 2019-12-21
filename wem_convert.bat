8208bf41bf499b2318befadcbe80271095382385
Maddoxkkm <maddox.kkm@gmail.com>
2019-12-22 01:23:50 +1100

    added ffmpeg

--- wem_convert.bat
+++ wem_convert.bat
@@ -1,15 +1,18 @@
 @echo off
 call :toOggTree
 call :revorbTree
+call :oggToWav
 goto :eof
 
-:events
-rem Callll
+
 
 
 :toOggTree
 rem Do whatever you want here over the files of this subdir, for example:
-for %%f in (*.wem) do %~dp0\ww2ogg\ww2ogg.exe %%f --pcb %~dp0\ww2ogg\packed_codebooks_aoTuV_603.bin
+for %%f in (*.wem) do (
+	%~dp0\ww2ogg\ww2ogg.exe %%f --pcb %~dp0\ww2ogg\packed_codebooks_aoTuV_603.bin
+	del %%f
+	)
 for /D %%d in (*) do (
     cd %%d
     call :toOggTree
@@ -25,6 +28,19 @@ for %%f in (*.ogg) do %~dp0\revorb\revorb.exe %%f
 for /D %%d in (*) do (
     cd %%d
     call :revorbTree
+    cd ../
+)
+exit /b
+
+:oggToWav
+rem Do whatever you want here over the files of this subdir, for example:
+for %%f in (*.ogg) do (
+	%~dp0\ffmpeg\ffmpeg.exe -i %%f %%~nf.wav
+	del %%f
+)
+for /D %%d in (*) do (
+    cd %%d
+    call :oggToWav
     cd ../
 )
 exit /b
